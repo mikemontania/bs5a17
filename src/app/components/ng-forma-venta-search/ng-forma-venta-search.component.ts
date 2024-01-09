67300a85c163b753e2b873543958fd63df983f94
Miguel Montania <mikemontania@gmail.com>
2024-01-09 17:27:54 -0300

    selectores

---
+++ src/app/components/ng-forma-venta-search/ng-forma-venta-search.component.ts
@@ -0,0 +1,57 @@
+import { CommonModule } from "@angular/common";
+import { Component,       EventEmitter, Input, OnInit, Output, ViewContainerRef, inject } from "@angular/core";
+import { debounceTime, distinctUntilChanged } from "rxjs";
+import { InputDebounceComponent } from "../inputDebounce/inputDebounce.component";
+import { Cliente } from '../../interfaces/clientes.interface';
+import { ClientesService } from "../../services/clientes.service";
+
+@Component({
+  selector: "app-forma-venta-search",
+  standalone: true,
+  imports: [CommonModule, InputDebounceComponent],
+  templateUrl: "./ng-forma-venta-search.component.html",
+  styleUrl: "./ng-forma-venta-search.component.css"
+})
+export class NgFormaVentaSearchComponent implements OnInit {
+  size = "medium";
+  delay=200;
+  @Input() isOpen = false;
+  @Output() closeModal = new EventEmitter<void>();
+  @Output() cliente = new EventEmitter<Cliente>();
+
+  clientes: Cliente[] = [];
+  _clientesService = inject(ClientesService);
+
+  ngOnInit(): void {
+    this.clientes =[];
+
+    this.buscar('');
+  }
+
+
+selectCliente(cliente: Cliente) {
+  this.cliente.emit(cliente);
+
+}
+trackCliente(index: number, cliente: Cliente): number {
+  return cliente.id; // Assuming cliente has a unique ID
+}
+
+
+  close() {
+    this.closeModal.emit();
+  }
+
+  buscar(termino: string) {
+
+    this._clientesService.searchClientes(1, 10, termino)
+    .pipe(debounceTime(1500), distinctUntilChanged())
+    .subscribe((response: any) => {
+      console.log(response);
+      this.clientes = response.rows as Cliente[];
+    });
+    /*   debounceTime(300);
+    distinctUntilChanged(); */
+    console.log(termino);
+  }
+}
